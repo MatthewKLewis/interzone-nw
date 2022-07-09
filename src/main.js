f2bbffd7320338f2a1bcde41d4753a43deebf227
Matthew Lewis <matthew.knight.lewis@gmail.com>
2022-07-09 13:12:45 -0400

    elevation

--- src/main.js
+++ src/main.js
@@ -16,6 +16,9 @@ const characterMenu = document.querySelector('#character');
 
 //proximity
 const timer = document.querySelector('#timer');
+const lookDisplay = document.querySelector('#look');
+
+//character
 const playerNameDisplay = document.querySelector('#player-name');
 
 const log = document.querySelector('#admin-log');
@@ -36,9 +39,14 @@ for (let i = 0; i < CONSONANTS.length; i++) {
 }
 
 let paused = true;
+let worldMap = true; // as opposed to region map
 let time = 0;
 let world = {};
+let currentRegion = {};
 let player = {};
+
+let mouseoverRegion = {};
+let mouseoverTile = {};
 
 function generateSciFiName(syllables = 3) {
     let retStr = ''
@@ -47,7 +55,6 @@ function generateSciFiName(syllables = 3) {
     }
     return retStr
 }
-function convertXYToTileIndex(x, y) { }
 //#endregion
 
 //#region [rgba(0,255,0,0.15)] ADMIN LOG
@@ -68,23 +75,67 @@ function addLog(text) {
 //#region [rgba(255,0,0,0.15)] CLASSES
 
 class World {
-    name = "Urth";
     regions = [];
-    constructor() {
+    constructor(regions = []) {
+        if (regions.length > 0) {
+            console.log("loading existing world")
+            this.regions = regions;
+        } else {
+            console.log("creating new world")
+            this.initialNoise();
+            this.smooth()
+        }
+    }
+
+    initialNoise() {
         for (let x = 0; x < (cnv.width / PIXEL_WIDTH); x++) {
             for (let y = 0; y < (cnv.height / PIXEL_WIDTH); y++) {
-                this.regions.push(new Region(x, y, Math.random() > 0.5 ? 1 : 0))
+                this.regions.push(new Region(x, y, Math.floor(Math.random() * 10)))
             }
         }
     }
+
+    smooth() {
+        console.log("neighbors:")
+        console.log(this.getNeighboringRegions(0))
+
+        for (let i = 0; i < this.regions.length; i++) {
+            let neighboringRegions = this.getNeighboringRegions(i);
+        }
+    }
+
+    //Utils:
+
+    getRegionAtMouse(x, y) {
+        let tileX = Math.floor(x / 16)
+        let tileY = Math.floor(y / 16)
+        return this.regions[tileX + (tileY * 88)]
+    }
+
+    getNeighboringRegions(regionIndex) {
+        let neiReg = []
+        neiReg[0] = this.regions[regionIndex - 88] || null; //NORTH
+        neiReg[1] = this.regions[regionIndex + 1 ] || null; //EAST
+        neiReg[2] = this.regions[regionIndex + 88] || null; //SOUTH
+        neiReg[3] = this.regions[regionIndex - 1 ] || null; //WEST
+        return neiReg;
+    }
+
 }
 
 class Region {
-    constructor(x, y, i) {
+    tiles = [];
+    constructor(x, y, elevation) {
         this.name = generateSciFiName(3);
         this.x = x;
         this.y = y;
-        this.i = i;
+        this.elevation = elevation;
+    }
+
+    getTileAtMouse(x, y) {
+        let tileX = Math.floor(x / 16)
+        let tileY = Math.floor(y / 16)
+        return this.tiles[tileX + (tileY * 88)]
     }
 }
 
@@ -127,9 +178,18 @@ cnv.addEventListener('click', (e)=>{
 cnv.addEventListener('mousemove', (e)=>{
     let pointerX = e.clientX - e.target.offsetLeft;
     let pointerY = e.clientY - e.target.offsetTop;
+
+    if (worldMap) {
+        mouseoverRegion = world.getRegionAtMouse(pointerX, pointerY);
+        lookDisplay.innerText = mouseoverRegion.name;
+    } else {
+        mouseoverTile = currentRegion.getTileAtMouse(pointerX, pointerY);
+    }
+
     let tileX = Math.floor(pointerX / 16)
     let tileY = Math.floor(pointerY / 16)
     addLog("mouse:" + tileX + ":" + tileY);
+
 })
 
 document.addEventListener('keydown', (e) => {
@@ -176,7 +236,7 @@ function continueGame() {
             console.log(err);
             return;
         }
-        world = JSON.parse(data);
+        world = new World(JSON.parse(data).regions);
         player = new Player('Matthew', 0, 0, true);
         toggleMainMenu()
         gameLoop();
@@ -243,8 +303,36 @@ function drawMenus() {
 
 function drawCanvas() {
     for (let i = 0; i < world.regions.length; i++) {
-        ctx.fillStyle = world.regions[i].i == 0 ? 'black' : 'white';
+        //world
+        ctx.fillStyle = "#" + world.regions[i].elevation + world.regions[i].elevation + world.regions[i].elevation;
+        switch (world.regions[i].elevation) {
+            case 1:
+                ctx.fillStyle = 'blue'                
+                break;        
+            case 2:                
+                break;        
+            case 3:                
+                break;        
+            case 4:                
+                break;        
+            case 5:                
+                break;        
+            case 6:                
+                break;        
+            case 7:
+                break;
+            case 8:             
+                break;
+            case 9:        
+            ctx.fillStyle = 'white'         
+                break;
+            default:
+                break;
+        }
+
         ctx.fillRect(world.regions[i].x * PIXEL_WIDTH, world.regions[i].y * PIXEL_WIDTH, PIXEL_WIDTH, PIXEL_WIDTH);
+
+        //player
         ctx.fillStyle = 'red';
         ctx.fillRect(player.x * PIXEL_WIDTH, player.y * PIXEL_WIDTH, PIXEL_WIDTH, PIXEL_WIDTH)
     }
