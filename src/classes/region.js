5d1fbee8bf54f1a76a7da4f3bd21783d5ce05490
Matthew Lewis <matthew.knight.lewis@gmail.com>
2022-07-14 18:20:33 -0400

    bldgs

--- src/classes/region.js
+++ src/classes/region.js
@@ -25,6 +25,9 @@ class Region {
             //do something here. take on a characterization. tell the tiles to do something.
             this.placePowerups();
 
+            //add npcs
+            this.placeDwellings();
+
             //finally determine the entry point
             this.entryPoint = this.determineEntryPoint();
 
@@ -51,45 +54,69 @@ class Region {
     initialNoise() {
         for (let y = 0; y < 64; y++) {
             for (let x = 0; x < 88; x++) {
-                this.tiles.push(new Tile(x, y, Math.random() > 0.99 ? 1 : 0, this.overworldTileUrl))
+                this.tiles.push(new Tile(x, y, 0, this.overworldTileUrl))
             }
-        }
-    }
-
-    determineEntryPoint() {
-        for (let i = 2000; i < this.tiles.length; i++) {
-            if (!this.tiles[i].wall) {
-                return [this.tiles[i].x, this.tiles[i].y]
-            }            
         }
     }
 
     placePowerups() {
         for (let y = 0; y < 64; y++) {
             for (let x = 0; x < 88; x++) {
-                Math.random() > 0.99 && this.items.push(new Weapon(x, y, 'Dagger', 'Increate'));
+                Math.random() > 0.999 && this.items.push(new Weapon(x, y, 'Dagger', 'Increate'));
             }
         }
     }
 
+    placeDwellings(iterations = 1) {
+        for (let i = 0; i < iterations; i++) {
+            let bldgWidth = this.randBetween(5, 10);
+            let bldgHeight = this.randBetween(5, 10);
+            let upLeftCorner = [this.randBetween(1, 87), this.randBetween(1, 63)];
+            let buildingFloorTiles = this.tiles.filter(tile =>
+                tile.x > upLeftCorner[0] && tile.x < upLeftCorner[0] + bldgWidth &&
+                tile.y > upLeftCorner[1] && tile.y < upLeftCorner[1] + bldgHeight
+            );
+            buildingFloorTiles.forEach(tile => {
+                tile.tileUrl = 'ice.png'
+            })
+            let buildingBorderTiles = this.tiles.filter(tile => 
+                (tile.x >= upLeftCorner[0] && tile.x <= upLeftCorner[0] + bldgWidth && (tile.y == upLeftCorner[1] || tile.y == upLeftCorner[1] + bldgHeight)) ||   //
+                (tile.y >= upLeftCorner[1] && tile.y <= upLeftCorner[1] + bldgHeight && (tile.x == upLeftCorner[0] || tile.x == upLeftCorner[0] + bldgWidth))
+            );
+            buildingBorderTiles.forEach(tile => {
+                tile.wall = Math.random() > 0.2 ? 1 : 0
+            })
+        }         
+    }
+
     assignImages() {
         for (let i = 0; i < this.tiles.length; i++) {
-            this.tiles[i].assignImage();            
+            this.tiles[i].assignImage();
         }
     }
 
+    //Utility
+    determineEntryPoint() {
+        for (let i = 2000; i < this.tiles.length; i++) {
+            if (!this.tiles[i].wall) {
+                return [this.tiles[i].x, this.tiles[i].y]
+            }
+        }
+    }
     getTileAtMouse(x, y) {
         let tileX = Math.floor(x / 16)
         let tileY = Math.floor(y / 16)
         return this.tiles[tileX + (tileY * 88)]
     }
-
     saveRegion() {
         fs.writeFileSync(`./assets/world/region${this.index}.json`, JSON.stringify(this), err => {
             if (err) {
                 //error condition
             }
         });
+    }
+    randBetween(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 }
 
